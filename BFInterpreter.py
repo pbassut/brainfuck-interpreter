ac057f375795c49c318bd132ec6ca38da6c54635
Patrick Souza <patrickbassut@hotmail.com>
2013-10-17 12:19:50 -0300

    Readme and main class
    Took out the examples from the main class file, and put it in the
    example file.

--- BFInterpreter.py
+++ BFInterpreter.py
@@ -102,15 +102,4 @@ class Interpreter():
             print 'Cell #%d: %s ' % (index, each)
 
 
-# Copies cell #1 to Cell #2 and prints the ASCII value of cell #2
-program = '++++++ [ > ++++++++++ < - ] > +++++ .'
-
-# Reads a input character and copies to cell #2
-#program = ', [ > + < - ] > .'
-
- # Reads two input characters, multiply them, stores the result on cell #3
-#program = ',>,< [ > [ >+ >+ << -] >> [- << + >>] <<< -] >>'
-
-# Makes the program run out of memory
-#program = '+[>+]'
-interpreter = Interpreter(program, True)
+interpreter = Interpreter('++++++ [ > ++++++++++ < - ] > +++++ .', True)
--- README.md
+++ README.md
@@ -2,3 +2,18 @@ Brainfuck-Interpreter
 =====================
 
 A simple Brainfuck interpter written in python
+
+
+EXAMPLES
+
+# Copies cell #1 to Cell #2 and prints the ASCII value of cell #2
+program = '++++++ [ > ++++++++++ < - ] > +++++ .'
+
+# Reads a input character and copies to cell #2
+program = ', [ > + < - ] > .'
+
+ # Reads two input characters, multiply them, stores the result on cell #3
+program = ',>,< [ > [ >+ >+ << -] >> [- << + >>] <<< -] >>'
+
+# Makes the program run out of memory
+program = '+[>+]'
